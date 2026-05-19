4fffb8834f8a8c4d89ba7992a097e98938a0a5a8
MZH-Work <mzh@google.com>
2026-05-19 11:07:16 +0800

    rm some build sh

--- build.sh
+++
@@ -1,2 +0,0 @@
-cargo build --release --target aarch64-unknown-linux-musl  --bins --examples
-cp build/cargo_target/aarch64-unknown-linux-musl/release/firecracker ~/cca/kata-firecracker/run/
--- cp-build.sh
+++
@@ -1,2 +0,0 @@
-cargo build --release --target aarch64-unknown-linux-musl  --bins --examples
-cp -v build/cargo_target/aarch64-unknown-linux-musl/release/firecracker /home/mzh/cca/gpu/GPU_SFTP/firecracker-bins/
